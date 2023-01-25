1b28c5197fad966b38c825af7152bfea95b3e15c
Ricardo <rickyfernandez210@gmail.com>
2023-01-24 22:00:24 -0300

    test

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Routes>
       <Route path="/" element={<ItemListContainer />} />
       <Route path="/category/:category" element={<ItemListContainer />} />
-      <Route path="item/:id" element={<ItemDetailContainer />} />
+      <Route path="/item/:id" element={<ItemDetailContainer />} />
     </Routes>
     </BrowserRouter>
   );    
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -4,7 +4,8 @@ import { useParams } from "react-router-dom";
 
 const ItemListContainer = ({ greeting }) => {
     const [products, setProducts] = useState ([]); 
-    const [filteredProducts, setFilteredProducts] = useState([]);
+    // esta te la elimino para no duplicar codigo lo hago en el use Effect
+    /* const [filteredProducts, setFilteredProducts] = useState([]); */
     const { category } = useParams();
 
     const getProducts = fetch('https://fakestoreapi.com/products', {
@@ -20,24 +21,29 @@ const ItemListContainer = ({ greeting }) => {
         return res.json()
       })
       .then((response) => {
-        setProducts(response);
-      }) 
-      .catch((error) => console.log(error));
-    }, []);
+        if(category){
+          const filterProduct = response.filter((p)=>p.category===category);
+          setProducts(filterProduct)
+        }else{setProducts(response)}
+      })
+      .catch((error) => console.log(error))
+    }, [category]);
+    
+    //lo unifique en la linea 26 todos los ternarios y condicionales y agrego al use effect el [category para que se active al cambiar de categorya]
 
-    useEffect(() => {
+    /* useEffect(() => {
       if(category) {
         console.log(category);
         const removeCharacters = category.includes('%20') ? category.replace('%20', '') : category;
         const filterProducts = products.filter((product) => product.category === removeCharacters );
         setFilteredProducts(filterProducts)
       }
-    }, [category]);
-
+    }, [category]); */
+    
     return (
       <div>
         {greeting}
-        <ItemList productos={category ? filteredProducts : products}/> 
+        <ItemList productos={products}/>
       </div>
     );
 
