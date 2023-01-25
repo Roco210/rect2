639f2227f806d38f68b02db16f001c58ecde1575
Ricardo <rickyfernandez210@gmail.com>
2023-01-24 22:45:02 -0300

    test2

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,10 +2,11 @@ import Item from "./Item";
 import { Link } from "react-router-dom";
 
 const ItemList = ({ productos }) => {
+  //en este punto te falto poner el slash de item, te generaba arriba de category 
   return (
     <ul>
       {productos.map((producto) => (
-        <Link key={producto.id} to={`/item/${producto.id}`}>
+        <Link key={producto.id} to={`/item/${producto.id}`}> 
           <Item producto={producto} />
         </Link>
       ))}
